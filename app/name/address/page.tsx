c0249b9d453bf013d7642183c2e3c7284ada0176
masakrambutt <msakrambutt@gmail.com>
2023-01-10 16:07:29 +0500

    second commit

--- app/name/address/page.tsx
+++ app/name/address/page.tsx
@@ -1,8 +1,12 @@
+import Link from 'next/link'
 export default function GiveAddress() {
     return (
       <div>
         <br></br>
-           <center><h1>I live in Lahore,</h1><br></br><h3>Pakistan</h3></center> 
+           <center><h1>I live in Lahore,</h1><br></br><h3>Pakistan</h3>
+           <br/>
+          <Link href="/"><b><i>Home Page</i></b></Link>
+          </center> 
       </div>
     )
   }
